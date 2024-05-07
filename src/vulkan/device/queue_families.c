959375774d47d573cbf884394b5314abec4cd88e
jarvarvarvis <jarvistrigo@gmail.com>
2024-05-07 21:37:47 +0200

    Proper output of queue families and supported flags as bit string

--- src/vulkan/device/queue_families.c
+++ src/vulkan/device/queue_families.c
@@ -2,6 +2,16 @@
 
 #include "../common.h"
 
+void vkt_convert_queue_flags_to_bit_chars(VkQueueFlags flags, char *bit_chars) {
+    for (int i = 0; i < 8; ++i) {
+        VkQueueFlags mask = 1 << i;
+        int bit = (flags & mask) >> i;
+
+        bit_chars[7-i] = (bit == 1) ? '1' : '0';
+    }
+    bit_chars[8] = '\0';
+}
+
 int vkt_find_queue_families_for_device(
     VkPhysicalDevice device,
     VkQueueFlagBits *families_to_find_flags,
@@ -29,7 +39,9 @@ int vkt_find_queue_families_for_device(
         // Iterate over queue families
         for (int family_index = 0; family_index < queue_family_count; ++family_index) {
             VkQueueFlags queue_flags = queue_families[family_index].queueFlags;
-            c_log(C_LOG_SEVERITY_DEBUG, "vkt_find_queue_families_for_device :: queue family %d has flags: %d", i, queue_flags);
+            char queue_flags_bit_str[9];
+            vkt_convert_queue_flags_to_bit_chars(queue_flags, queue_flags_bit_str);
+            c_log(C_LOG_SEVERITY_DEBUG, "vkt_find_queue_families_for_device :: queue family %d has flags: %s", family_index, queue_flags_bit_str);
 
             // If the required flags are supported, set family index in output
             if ((queue_flags & required_flags) == required_flags) {
--- src/vulkan/device/queue_families.h
+++ src/vulkan/device/queue_families.h
@@ -3,6 +3,8 @@
 
 #include <vulkan/vulkan.h>
 
+void vkt_convert_queue_flags_to_bit_chars(VkQueueFlags flags, char *bit_chars);
+
 int vkt_find_queue_families_for_device(
     VkPhysicalDevice device,
     VkQueueFlagBits *families_to_find_flags,
