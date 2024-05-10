29318192a32eda17b48ec13efb904bf8f7889883
jarvarvarvis <jarvistrigo@gmail.com>
2024-05-10 02:38:59 +0200

    Add message severity and type info to debug utils logging

--- src/vulkan/instance/debug.c
+++ src/vulkan/instance/debug.c
@@ -1,14 +1,50 @@
 #include "debug.h"
 
+#include <vulkan/vk_enum_string_helper.h>
+
 #include <c_log/c_log.h>
 
+static inline const char* vkt_string_VkDebugUtilsMessageSeverityFlagBitsEXT(VkDebugUtilsMessageSeverityFlagBitsEXT input_value) {
+    switch (input_value) {
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT:
+            return "VERBOSE";
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT:
+            return "INFO";
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:
+            return "WARNING";
+        case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:
+            return "ERROR";
+        default:
+            return "<Unknown>";
+    }
+}
+
+static inline const char* vkt_string_VkDebugUtilsMessageTypeFlagBitsEXT(VkDebugUtilsMessageTypeFlagBitsEXT input_value) {
+    switch (input_value) {
+        case VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT:
+            return "GENERAL";
+        case VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT:
+            return "VALIDATION";
+        case VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT:
+            return "PERFORMANCE";
+        case VK_DEBUG_UTILS_MESSAGE_TYPE_DEVICE_ADDRESS_BINDING_BIT_EXT:
+            return "ADDRESS_BINDING";
+        default:
+            return "Unhandled VkDebugUtilsMessageTypeFlagBitsEXT";
+    }
+}
+
 static VKAPI_ATTR VkBool32 VKAPI_CALL vkt_debug_callback(
-    VkDebugUtilsMessageSeverityFlagBitsEXT message_severity,
+    VkDebugUtilsMessageSeverityFlagsEXT message_severity,
     VkDebugUtilsMessageTypeFlagsEXT message_type,
 	const VkDebugUtilsMessengerCallbackDataEXT *callback_data,
 	void *user_data
 ) {
-	c_log(C_LOG_SEVERITY_DEBUG, "Vulkan Validation :: %s", callback_data->pMessage);
+	c_log(C_LOG_SEVERITY_DEBUG, "Vulkan Validation :: Severity = %s, Type = %s",
+        vkt_string_VkDebugUtilsMessageSeverityFlagBitsEXT(message_severity),
+        vkt_string_VkDebugUtilsMessageTypeFlagBitsEXT(message_type)
+    );
+	c_log(C_LOG_SEVERITY_DEBUG, "%s", callback_data->pMessage);
 	return VK_FALSE;
 }
 
