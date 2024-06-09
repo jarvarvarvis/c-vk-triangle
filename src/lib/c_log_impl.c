bee7321ffcc04da0586dd410f8a0b9d5d2b57800
jarvarvarvis <jarvistrigo@gmail.com>
2024-06-10 00:18:16 +0200

    Provide stb_image implementation, move vma/impl.cpp to vma_impl.cpp

--- src/lib/c_log_impl.c
+++
@@ -1,2 +0,0 @@
-#define C_LOG_IMPLEMENTATION
-#include <c_log/c_log.h>
---
+++ src/lib/others_impl.c
@@ -0,0 +1,5 @@
+#define C_LOG_IMPLEMENTATION
+#include <c_log/c_log.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
